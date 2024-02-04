2333daf4747563dc0af49d9dacc9da49bbc856c3
prafullKrRj <prafullkumar384@gmail.com>
2024-02-04 15:54:46 +0530

    version 1 completed

--- app/src/main/java/com/prafullkumar/campusepulse/commons/TimeTableImage.kt
+++ app/src/main/java/com/prafullkumar/campusepulse/commons/TimeTableImage.kt
@@ -1,9 +1,11 @@
 package com.prafullkumar.campusepulse.commons
 
-import androidx.compose.foundation.gestures.detectTransformGestures
-import androidx.compose.foundation.layout.Box
+import android.util.Log
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
@@ -11,63 +13,60 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import coil.request.SuccessResult
 import com.prafullkumar.campusepulse.R
 
 @Composable
 fun TimeTableImage(data: String) {
     var scale by remember { mutableFloatStateOf(1f) }
     var offset by remember { mutableStateOf(Offset.Zero) }
-    var originalSize by remember { mutableStateOf(IntSize.Zero) }
-
-    val modifier = Modifier
-        .graphicsLayer(
-            scaleX = maxOf(1f, scale),
-            scaleY = maxOf(1f, scale),
-            translationX = offset.x,
-            translationY = offset.y
-        )
-        .pointerInput(Unit) {
-            detectTransformGestures { _, pan, zoom, _ ->
-                scale *= zoom
-                scale = maxOf(1f, scale)
-                offset = if (scale == 1f) {
-                    Offset.Zero
-                } else {
-                    val newOffset = offset + pan
-                    Offset(
-                        x = newOffset.x.coerceIn(-originalSize.width * (scale - 1), originalSize.width * (scale - 1)),
-                        y = newOffset.y.coerceIn(-originalSize.height * (scale - 1), originalSize.height * (scale - 1))
-                    )
-                }
+    var aspectRatio by remember { mutableFloatStateOf(1f) }
+    Log.d("aspectRatio", aspectRatio.toString())
+    val request = ImageRequest.Builder(LocalContext.current)
+        .data(data)
+        .listener(object : ImageRequest.Listener {
+            override fun onSuccess(request: ImageRequest, result: SuccessResult) {
+                aspectRatio = result.drawable.intrinsicWidth.toFloat() / result.drawable.intrinsicHeight.toFloat()
+                Log.d("aspectRatio", aspectRatio.toString())
             }
+        })
+        .build()
+    BoxWithConstraints(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(aspectRatio)
+            .clipToBounds()
+    ) {
+        val state = rememberTransformableState { zoomChange, panChange, rotationChange ->
+            scale = (scale * zoomChange).coerceIn(1f, 4f)
+            val extraWidth = (scale - 1) * constraints.maxWidth
+            val extraHeight = (scale - 1) * constraints.maxHeight
+            val maxX = (extraWidth / 2)
+            val maxY = (extraHeight / 2)
+            offset = Offset(
+                x = (offset.x + panChange.x).coerceIn(-maxX, maxX),
+                y = (offset.y + panChange.y).coerceIn(-maxY, maxY)
+            )
         }
-
-    Box(modifier = modifier) {
         AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current).data(data).build(),
+            model = request,
             contentDescription = null,
-            contentScale = ContentScale.Fit,
             modifier = Modifier
-                .padding(16.dp)
                 .fillMaxWidth()
-                .onSizeChanged { size ->
-                    if (originalSize == IntSize.Zero) {
-                        originalSize = size
-                    }
-                    val minScale = minOf(size.width.toFloat() / originalSize.width, size.height.toFloat() / originalSize.height)
-                    scale = maxOf(scale, minScale)
-                },
+                .graphicsLayer {
+                    scaleX = scale
+                    scaleY = scale
+                    translationX = offset.x
+                    translationY = offset.y
+                }
+                .transformable(state),
             placeholder = painterResource(id = R.drawable.loading_img),
             error = painterResource(id = R.drawable.ic_broken_image)
         )
